24a2ee23fc366e815277cde514ea26944487bb37
Timo Deuschle <timo.deuschle@kaiserkraft-europa.de>
2018-10-19 16:06:23 +0200

    use redux-logger instead of custom one

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,14 +1,20 @@
-import { Store, createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { routerMiddleware } from 'react-router-redux';
-import { History } from 'history';
-import { logger } from 'app/middleware';
-import { RootState, rootReducer } from 'app/reducers';
+import {applyMiddleware, createStore, Store} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {routerMiddleware} from 'react-router-redux';
+import {History} from 'history';
+import logger from 'redux-logger';
+import {rootReducer, RootState} from 'app/reducers';
 
 export function configureStore(history: History, initialState?: RootState): Store<RootState> {
-  let middleware = applyMiddleware(logger, routerMiddleware(history));
+  const isProd = process.env.NODE_ENV === 'production';
+  const middlewares = [routerMiddleware(history)];
+  if (!isProd) {
+    middlewares.push(logger);
+  }
 
-  if (process.env.NODE_ENV !== 'production') {
+  let middleware = applyMiddleware(...middlewares);
+
+  if (!isProd) {
     middleware = composeWithDevTools(middleware);
   }
 
