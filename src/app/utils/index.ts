cfc3b73ee9496b8454ade2604bb1532238f6d006
Timo Deuschle <timo.deuschle@kaiserkraft-europa.de>
2018-10-23 17:02:20 +0200

    (refactor) getting rid of ToDo app

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,13 +1,1 @@
-export function omit<T extends object, K extends keyof T>(target: T, ...omitKeys: K[]): Omit<T, K> {
-  return (Object.keys(target) as K[]).reduce(
-    (res, key) => {
-      if (!omitKeys.includes(key)) {
-        res[key] = target[key];
-      }
-      return res;
-    },
-    {} as any
-  );
-}
-
 export const isProduction = () => process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
